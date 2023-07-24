460a5ff8dd1f2d32b07dd4c58a07122ac60480f8
Steven Diestel <82208488+Izamii@users.noreply.github.com>
2023-07-24 03:42:49 +0200

    Changed request mapping uri to work with the api gateway

--- src/main/java/edu/timebandit/BasketService/port/checkout/controller/CheckoutBasketController.java
+++ src/main/java/edu/timebandit/BasketService/port/checkout/controller/CheckoutBasketController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/bs/api/v1")
 public class CheckoutBasketController {
 
     @Autowired
